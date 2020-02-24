fd0c88320d54f6f0ac8e7c7e63a137711a64747a
khacpv <khacpv@gmail.com>
2020-02-25 00:48:37 +0700

    init website

---
+++ precache-manifest.c5632392350987b257d0e653263224e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb87608b89cea0729abd3f7ffa8a6e8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8655bd50f0311067b7ee",
+    "url": "/static/css/main.da2aefca.chunk.css"
+  },
+  {
+    "revision": "81cb03aacb725ae4f900",
+    "url": "/static/js/2.9955dcdb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9955dcdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8655bd50f0311067b7ee",
+    "url": "/static/js/main.78772211.chunk.js"
+  },
+  {
+    "revision": "018cccec5be0a1208cae",
+    "url": "/static/js/runtime-main.32421873.js"
+  }
+]);
